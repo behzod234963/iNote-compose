d3258f650a9b77216013a588717ac7db3d8d3c20
Behzod <behzod96447@internet.ru>
2024-02-15 22:48:55 +0300

    SplashScreen

--- app/src/main/java/coder/bekhzod/presentation/activities/MainActivity.kt
+++ app/src/main/java/coder/bekhzod/presentation/activities/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import coder.bekhzod.presentation.screens.EmptyMainScreen
+import coder.bekhzod.presentation.screens.SplashScreen
 import coder.bekhzod.presentation.ui.theme.INoteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EmptyMainScreen()
+            SplashScreen()
         }
     }
 }
---
+++ app/src/main/java/coder/bekhzod/presentation/screens/SplashScreen.kt
@@ -0,0 +1,44 @@
+package coder.bekhzod.presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coder.bekhzod.R
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun SplashScreen() {
+    val notesAnimComposition = rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(resId = R.raw.notes)
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        LottieAnimation(
+            modifier = Modifier
+                .size(250.dp),
+            composition = notesAnimComposition.value,
+            isPlaying = true,
+            restartOnPlay = true,
+            reverseOnRepeat = true,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen() {
+    SplashScreen()
+}
