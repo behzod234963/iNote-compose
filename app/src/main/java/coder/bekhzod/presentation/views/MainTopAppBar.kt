9f7d3e9c8c1bcafdf2b9c60029b7b7d1c95b61ca
Behzod <behzod96447@internet.ru>
2024-03-23 19:48:06 +0300

    Main and EmptyMainScreen

--- app/src/main/java/coder/bekhzod/presentation/views/MainTopAppBar.kt
+++ app/src/main/java/coder/bekhzod/presentation/views/MainTopAppBar.kt
@@ -2,14 +2,15 @@ package coder.bekhzod.presentation.views
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -27,9 +28,10 @@ import coder.bekhzod.R
 import coder.bekhzod.presentation.ui.theme.fontAmidoneGrotesk
 import coder.bekhzod.presentation.utils.constants.PassDataEvents
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopAppBar() {
-    val isOpened = remember{
+    val isOpened = remember {
         mutableStateOf(false)
     }
     Box(
@@ -44,9 +46,9 @@ fun MainTopAppBar() {
                 .padding(top = 10.dp, start = 5.dp, end = 5.dp),
             contentAlignment = Alignment.TopStart
         ) {
-            AnimatedDropDownMenu {event->
-                when(event){
-                    is PassDataEvents.PassStatus->{
+            AnimatedDropDownMenu { event ->
+                when (event) {
+                    is PassDataEvents.PassStatus -> {
                         isOpened.value = event.status
                     }
                 }
@@ -64,12 +66,12 @@ fun MainTopAppBar() {
                 }
             }
         }
-        Box (
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 10.dp),
             contentAlignment = Alignment.TopCenter
-        ){
+        ) {
             Text(
                 text = "iNote",
                 color = Color.White,
@@ -82,19 +84,15 @@ fun MainTopAppBar() {
             modifier = Modifier
                 .fillMaxWidth(),
             contentAlignment = Alignment.TopEnd
-        ){
-            IconButton(
-                onClick = {
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_more),
+                modifier = Modifier.clickable {
 
-                }
-            ) {
-               Icon(
-                   painter = painterResource(
-                   id = R.drawable.ic_more),
-                   tint = Color.White,
-                   contentDescription = "icon more settings"
-               )
-            }
+                },
+                tint = Color.White,
+                contentDescription = "icon more settings"
+            )
         }
     }
 }
--- app/src/main/java/coder/bekhzod/presentation/views/MoreDropDownMenu.kt
+++ app/src/main/java/coder/bekhzod/presentation/views/MoreDropDownMenu.kt
@@ -1,24 +1,105 @@
 package coder.bekhzod.presentation.views
 
+import android.app.LocaleConfig
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoreDropDownMenu(
-    onEditClick:() -> Unit ,
-    onClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onSettingsClick: () -> Unit,
     onDeleteClick: () -> Unit,
 ) {
-
-    onClick.invoke()
-    onEditClick.invoke()
-    onDeleteClick.invoke()
+    val functionsList = mutableListOf("Edit", "Delete", "Settings")
+    val isExpanded = remember { mutableStateOf(false) }
+    val isSelected = remember { false }
+    var selectedItem = remember { functionsList[0] }
+    ExposedDropdownMenuBox(
+        modifier = Modifier
+            .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(10.dp)),
+        expanded = isExpanded.value,
+        onExpandedChange = { isExpanded.value = !isExpanded.value }
+    ) {
+        OutlinedTextField(
+            enabled = true,
+            readOnly = true,
+            textStyle = TextStyle(fontSize = 20.sp, color = Color.Black),
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded.value) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor()
+                .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(10.dp)),
+            value = selectedItem,
+            onValueChange = {}
+        )
+        ExposedDropdownMenu(
+            expanded = isExpanded.value,
+            onDismissRequest = { isExpanded.value = false }
+        ) {
+            for (i in functionsList.indices){
+                DropdownMenuItem(
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                modifier = Modifier.weight(1f),
+                                text = functionsList[i]
+                            )
+                        }
+                    },
+                    onClick = {
+                        if (functionsList[i].contains("Edit")){
+                            onEditClick.invoke()
+                        }else if (functionsList[i].contains("Delete")){
+                            onDeleteClick.invoke()
+                        }else if (functionsList[i].contains("Settings")){
+                            onSettingsClick.invoke()
+                        }
+                        isExpanded.value = false
+                        selectedItem = i.toString()
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun PreviewMoreDDM() {
-    MoreDropDownMenu(onEditClick = { /*TODO*/ }, onClick = { /*TODO*/ }, onDeleteClick = { TODO() })
+    val ctx = LocalContext.current.applicationContext
+    MoreDropDownMenu(
+        onEditClick = { Toast.makeText(ctx, "Edit is Clicked", Toast.LENGTH_SHORT).show() },
+        onSettingsClick = { Toast.makeText(ctx, "Settings is Clicked", Toast.LENGTH_SHORT).show() },
+        onDeleteClick = { Toast.makeText(ctx, "Delete is Clicked", Toast.LENGTH_SHORT).show() }
+    )
 }
